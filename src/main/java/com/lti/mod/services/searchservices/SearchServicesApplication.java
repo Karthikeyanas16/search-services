bd8392649e5746ac6fa377931b997f1b3eb420a3
Karthikeyan <karthikeyan.as@lntinfotech.com>
2020-08-02 20:03:19 +0530

    initial commit

---
+++ src/main/java/com/lti/mod/services/searchservices/SearchServicesApplication.java
@@ -0,0 +1,13 @@
+package com.lti.mod.services.searchservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServicesApplication.class, args);
+	}
+
+}
