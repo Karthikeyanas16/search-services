11503ba10776e07fbec5900efa93e6f046a1e59a
dhoRam <dhondiram.jadhav@lntinfotech.com>
2020-08-04 12:12:14 +0530

    removed web-security

--- src/main/java/com/lti/mod/services/searchservices/controller/SearchController.java
+++ src/main/java/com/lti/mod/services/searchservices/controller/SearchController.java
@@ -7,26 +7,21 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.lti.mod.services.searchservices.model.Course;
 import com.lti.mod.services.searchservices.service.SearchService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class SearchController {
-	
+
 	@Autowired
 	SearchService searchService;
-	
-	@GetMapping("/courses")
+
+	@RequestMapping(value = "/courses", method = RequestMethod.GET)
     public List<Course> findAllCourses() {
-        return searchService.findAll();
+        System.out.println("Search service ####");
+		return searchService.findAll();
     }
 	
 	@GetMapping("/courses/search/technology")
